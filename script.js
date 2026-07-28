c08aeff4b3904bf1d174049c3b0e1c883f947dc6
Akash Pramanik <akashpramanik422@gmail.com>
2026-07-29 02:10:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,9 +16,9 @@
   ];
 
   const VEHICLES = [
-    { id: "flappy1", name: "Rickshaw", cost: 0,   img: "images/flappy1.png" },
-    { id: "flappy2", name: "Scooter",  cost: 80,  img: "images/flappy2.png" },
-    { id: "flappy3", name: "Cycle",    cost: 150, img: "images/flappy3.png" },
+    { id: "flappy1", name: "Wing", cost: 0,   img: "images/flappy1.png" },
+    { id: "flappy2", name: "Helicopter",  cost: 80,  img: "images/flappy2.png" },
+    { id: "flappy3", name: "Tejas",    cost: 150, img: "images/flappy3.png" },
   ];
 
   const GRAVITY = 1500;         // px/s^2
